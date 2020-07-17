1781975999b8f21a51d02ef478960ba625b44f47
badaev <badaev777@yandex.ua>
2020-07-17 20:35:52 +0300

    Dashboard, update

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {ActivatedRoute} from "@angular/router";
+import {switchMap} from "rxjs/operators";
+import {Post} from "../../shared/interfaces";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-page',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params) => {
+        return this.postsService.getById(params['id'])
+      })
+    ).subscribe((post:Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required)
+        })
+    })
   }
 
+  submit() {
+
+  }
 }
